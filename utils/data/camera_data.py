84db2fe44c6123810e85d3cb3f36fc290edb5d7e
Sulabh Kumra <42680431+skumra@users.noreply.github.com>
2019-08-04 12:42:22 -0700

    Added utils

---
+++ utils/data/camera_data.py
@@ -0,0 +1,72 @@
+import glob
+import os
+import torch
+import numpy as np
+
+from PIL.Image import fromarray
+from utils.dataset_processing import grasp, image
+from .grasp_data import GraspDatasetBase
+
+
+class CameraData:
+    """
+    Dataset wrapper for the camera data.
+    """
+    def __init__(self, output_size=300, include_depth=True, include_rgb=True):
+        """
+        :param output_size: Image output size in pixels (square)
+        :param include_depth: Whether depth image is included
+        :param include_rgb: Whether RGB image is included
+        """
+        self.output_size = output_size
+        self.include_depth = include_depth
+        self.include_rgb = include_rgb
+
+        if include_depth is False and include_rgb is False:
+            raise ValueError('At least one of Depth or RGB must be specified.')
+
+    @staticmethod
+    def numpy_to_torch(s):
+        if len(s.shape) == 2:
+            return torch.from_numpy(np.expand_dims(s, 0).astype(np.float32))
+        else:
+            return torch.from_numpy(s.astype(np.float32))
+
+    def get_depth(self, img):
+        depth_img = image.Image(img)
+        depth_img.normalise()
+        depth_img.resize((self.output_size, self.output_size))
+        depth_img.img = depth_img.img.transpose((2, 0, 1))
+        return depth_img.img
+
+    def get_rgb(self, img, norm=True):
+        rgb_img = image.Image(img)
+        rgb_img.resize((self.output_size, self.output_size))
+        if norm:
+                rgb_img.normalise()
+                rgb_img.img = rgb_img.img.transpose((2, 0, 1))
+        return rgb_img.img
+
+    def get_data(self, rgb=None, depth=None):
+        # Load the depth image
+        if self.include_depth:
+            depth_img = self.get_depth(img=depth)
+
+        # Load the RGB image
+        if self.include_rgb:
+            rgb_img = self.get_rgb(img=rgb)
+
+        if self.include_depth and self.include_rgb:
+            x = self.numpy_to_torch(
+                    np.concatenate(
+                        (np.expand_dims(depth_img, 0),
+                         np.expand_dims(rgb_img, 0)),
+                        1
+                    )
+                )
+        elif self.include_depth:
+            x = self.numpy_to_torch(depth_img)
+        elif self.include_rgb:
+            x = self.numpy_to_torch(rgb_img)
+
+        return x, depth_img, rgb_img
