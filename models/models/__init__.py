3cc05f064c3e3e582272b9d32093ccd044fd3d9c
Sulabh Kumra <42680431+skumra@users.noreply.github.com>
2019-08-04 13:13:02 -0700

    Delete __init__.py

--- models/models/__init__.py
+++
@@ -1,7 +0,0 @@
-def get_network(network_name):
-    network_name = network_name.lower()
-    if network_name == 'sscnn':
-        from .sscnn import SSCNN
-        return SSCNN
-    else:
-        raise NotImplementedError('Network {} is not implemented'.format(network_name))
