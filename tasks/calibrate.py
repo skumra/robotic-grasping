12e9154512e8effc374f7dd1f390d39243ba8fd2
Sulabh Kumra <sulabh@osaro.com>
2019-12-29 12:27:01 -0800

    Use camera instead of camera2

--- tasks/calibrate.py
+++ tasks/calibrate.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
 
-from interfaces.camera2 import Camera
+from interfaces.camera import Camera
 from interfaces.robot import Robot
 
 
