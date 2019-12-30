3eb2052866768404fbc3cd5afc558946cafec9f2
Sulabh Kumra <sulabh@osaro.com>
2019-12-29 16:05:09 -0800

    Added import

--- tasks/calibrate.py
+++ tasks/calibrate.py
@@ -4,6 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 from interfaces.camera import RealSenseCamera
 from interfaces.robot import Robot
