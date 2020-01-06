1a5ee816bfab3b90669b8a4e03af12089f17a043
Sulabh Kumra <sulabh@osaro.com>
2020-01-06 11:50:30 -0800

    Updated get cornell

--- utils/get_cornell.sh
+++ utils/get_cornell.sh
@@ -2,11 +2,11 @@
 
 END=9
 for ((i=1;i<=END;i++)); do
-  curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data0$i.tar.gz
+  curl -O --retry 5 --max-time 15 http://pr.cs.cornell.edu/grasping/rect_data/temp/data0$i.tar.gz
   tar xvzf data0$i.tar.gz
   rm data0$i.tar.gz
 done
 
-curl http://pr.cs.cornell.edu/grasping/rect_data/temp/data10.tar.gz
+curl -O --retry 5 --max-time 15 http://pr.cs.cornell.edu/grasping/rect_data/temp/data10.tar.gz
 tar xvzf data10.tar.gz
 rm data10.tar.gz
