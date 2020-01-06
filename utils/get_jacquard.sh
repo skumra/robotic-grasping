9df58e83a751f1cbfe37a2ef1014af3e42e09523
Sulabh Kumra <sulabh@osaro.com>
2020-01-06 15:45:55 -0800

    Switched curl with wget

--- utils/get_jacquard.sh
+++ utils/get_jacquard.sh
@@ -2,7 +2,7 @@
 
 END=11
 for ((i=0;i<=END;i++)); do
-  curl -O --retry 5 --max-time 15 https://jacquard.liris.cnrs.fr/data/Download/Jacquard_Dataset_$i.zip
+  wget --retry 5 https://jacquard.liris.cnrs.fr/data/Download/Jacquard_Dataset_$i.zip
   unzip Jacquard_Dataset_$i.zip
   rm Jacquard_Dataset_$i.zip
 done
