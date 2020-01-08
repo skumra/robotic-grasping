d45175fc5bc362efc7e946267fd380d2410a1c86
Sulabh Kumra <sulabh@osaro.com>
2020-01-08 12:21:16 -0800

    Moved out robot related code

--- run_calibration.py
+++
@@ -1,5 +0,0 @@
-from tasks.calibrate import Calibration
-
-if __name__ == '__main__':
-    calibration = Calibration()
-    calibration.run()
--- run_realtime.py
+++ run_realtime.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch.utils.data
 
-from interfaces.camera import RealSenseCamera
+from hardware.camera import RealSenseCamera
 from inference.post_process import post_process_output
 from utils.data.camera_data import CameraData
 from utils.visualisation.plot import save_results, plot_grasp
