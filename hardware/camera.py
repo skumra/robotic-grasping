ae367777d1362ecb319e6b497afa1d8db29be808
Sulabh Kumra <sulabh@osaro.com>
2020-02-22 15:27:34 -0500

    Refactored camera

--- hardware/camera.py
+++ hardware/camera.py
@@ -10,21 +10,16 @@ logger = logging.getLogger(__name__)
 class RealSenseCamera:
     def __init__(self,
                  device_id,
-                 width_rgb=1280,
-                 height_rgb=720,
-                 width_depth=1280,
-                 height_depth=720,
-                 fps=30):
+                 width=640,
+                 height=480,
+                 fps=6):
 
         self.device_id = device_id
-        self.width_rgb = width_rgb
-        self.height_rgb = height_rgb
-        self.width_depth = width_depth
-        self.height_depth = height_depth
+        self.width = width
+        self.height = height
         self.fps = fps
 
         self.pipeline = None
-        self.align_to_rgb = None
         self.scale = None
         self.intrinsics = None
 
@@ -33,45 +28,39 @@ class RealSenseCamera:
         self.pipeline = rs.pipeline()
         config = rs.config()
         config.enable_device(str(self.device_id))
-        config.enable_stream(rs.stream.depth, self.width_depth, self.height_depth, rs.format.z16, self.fps)
-        config.enable_stream(rs.stream.color, self.width_rgb, self.height_rgb, rs.format.rgb8, self.fps)
+        config.enable_stream(rs.stream.depth, self.width, self.height, rs.format.z16, self.fps)
+        config.enable_stream(rs.stream.color, self.width, self.height, rs.format.rgb8, self.fps)
         cfg = self.pipeline.start(config)
 
         # Determine intrinsics
-        self.scale = cfg.get_device().first_depth_sensor().get_depth_scale()
         rgb_profile = cfg.get_stream(rs.stream.color)
-        depth_profile = cfg.get_stream(rs.stream.depth)
         rgb_intrinsics = rgb_profile.as_video_stream_profile().get_intrinsics()
-        depth_intrinsics = depth_profile.as_video_stream_profile().get_intrinsics()
-
         self.intrinsics = np.array([
             [rgb_intrinsics.fx, 0.0, rgb_intrinsics.ppx],
             [0.0, rgb_intrinsics.fy, rgb_intrinsics.ppy],
             [0.0, 0.0, 1.0]
         ])
 
-        # Alignment processors
-        self.align_to_rgb = rs.align(rs.stream.color)
+        # Determine depth scale
+        self.scale = cfg.get_device().first_depth_sensor().get_depth_scale()
 
     def get_image_bundle(self):
         frames = self.pipeline.wait_for_frames()
 
-        # RGB
-        cf = frames.get_color_frame()
-        rgb = np.asanyarray(cf.get_data())
+        align = rs.align(rs.stream.color)
+        aligned_frames = align.proccess(frames)
+        color_frame = aligned_frames.first(rs.stream.color)
+        aligned_depth_frame = aligned_frames.get_depth_frame()
 
-        # Depth aligned to RGB
-        color_aligned_frames = self.align_to_rgb.process(frames)
-        aligned_depth_frame = color_aligned_frames.get_depth_frame()
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        depth_image *= self.scale
+        color_image = np.asanyarray(color_frame.get_data())
 
-        rgb_aligned_depth = np.asarray(aligned_depth_frame.get_data(), dtype=np.float32)
-        rgb_aligned_depth *= self.scale * 1000.0
-
-        depth = np.expand_dims(rgb_aligned_depth, axis=2)
+        depth_image = np.expand_dims(depth_image, axis=2)
 
         return{
-            'rgb': rgb,
-            'aligned_depth': depth,
+            'rgb': color_image,
+            'aligned_depth': depth_image,
         }
 
     def plot_image_bundle(self):
