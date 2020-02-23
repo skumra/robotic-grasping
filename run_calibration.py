265a084094d66ae7fa4685a7eb201f08abf0ffbf
Shirin <saj8732@rit.edu>
2020-02-23 15:39:33 -0800

    updated checkerboard location

--- run_calibration.py
+++ run_calibration.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     calibration = Calibration(
         cam_id=830112070066,
         calib_grid_step=0.05,
-        checkerboard_offset_from_tool=[0.0, -0.113, 0.060],
+        checkerboard_offset_from_tool=[0.0, 0.022, 0.009],
         workspace_limits=np.asarray([[0.55, 0.65], [-0.2, -0.1], [0.0, 0.2]])  # Cols: min max, Rows: x y z (define workspace limits in robot coordinates)
     )
     calibration.run()
