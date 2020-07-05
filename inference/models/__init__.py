050af02ee0baee9c9784a2caf22e23e8138aa70a
Sulabh Kumra <sulabh@osaro.com>
2020-07-05 13:23:18 -0700

    Added configurable GR-ConvNets

---
+++ inference/models/__init__.py
@@ -0,0 +1,16 @@
+def get_network(network_name):
+    network_name = network_name.lower()
+    if network_name == 'grconvnet':
+        from .grconvnet import GenerativeResnet
+        return GenerativeResnet
+    elif network_name == 'grconvnet2':
+        from .grconvnet2 import GenerativeResnet
+        return GenerativeResnet
+    elif network_name == 'grconvnet3':
+        from .grconvnet3 import GenerativeResnet
+        return GenerativeResnet
+    elif network_name == 'grconvnet4':
+        from .grconvnet4 import GenerativeResnet
+        return GenerativeResnet
+    else:
+        raise NotImplementedError('Network {} is not implemented'.format(network_name))
--- train_network.py
+++ train_network.py
@@ -12,8 +12,8 @@ import torch.optim as optim
 import torch.utils.data
 from torchsummary import summary
 
-from inference.model import GenerativeResnet
 from hardware.device import get_device
+from inference.models import get_network
 from inference.post_process import post_process_output
 from utils.data import get_dataset
 from utils.dataset_processing import evaluation
@@ -23,25 +23,52 @@ from utils.visualisation.gridshow import gridshow
 def parse_args():
     parser = argparse.ArgumentParser(description='Train network')
 
+    # Network
+    parser.add_argument('--network', type=str, default='grconvnet',
+                        help='Network name in inference/models')
+    parser.add_argument('--use-depth', type=int, default=1,
+                        help='Use Depth image for training (1/0)')
+    parser.add_argument('--use-rgb', type=int, default=1,
+                        help='Use RGB image for training (1/0)')
+    parser.add_argument('--use-dropout', type=int, default=0,
+                        help='Use dropout for training (0/1)')
+    parser.add_argument('--dropout-prob', type=float, default=0.1,
+                        help='Dropout prob for training (0-1)')
+    parser.add_argument('--channel-size', type=int, default=32,
+                        help='Internal channel size for the network')
+
     # Dataset & Data & Training
-    parser.add_argument('--dataset', type=str, help='Dataset Name ("cornell" or "jaquard")')
-    parser.add_argument('--dataset-path', type=str, help='Path to dataset')
-    parser.add_argument('--use-depth', type=int, default=1, help='Use Depth image for training (1/0)')
-    parser.add_argument('--use-rgb', type=int, default=0, help='Use RGB image for training (0/1)')
-    parser.add_argument('--split', type=float, default=0.9, help='Fraction of data for training (remainder is validation)')
-    parser.add_argument('--ds-rotate', type=float, default=0.0, help='Shift the start point of the dataset to use a different test/train split for cross validation.')
-    parser.add_argument('--num-workers', type=int, default=8, help='Dataset workers')
-    parser.add_argument('--batch-size', type=int, default=8, help='Batch size')
-    parser.add_argument('--epochs', type=int, default=30, help='Training epochs')
-    parser.add_argument('--batches-per-epoch', type=int, default=1000, help='Batches per Epoch')
-    parser.add_argument('--val-batches', type=int, default=250, help='Validation Batches')
-    parser.add_argument('--optim', type=str, default='adam', help='Optmizer for the training. (adam or SGD)')
+    parser.add_argument('--dataset', type=str,
+                        help='Dataset Name ("cornell" or "jaquard")')
+    parser.add_argument('--dataset-path', type=str,
+                        help='Path to dataset')
+    parser.add_argument('--split', type=float, default=0.9,
+                        help='Fraction of data for training (remainder is validation)')
+    parser.add_argument('--ds-rotate', type=float, default=0.0,
+                        help='Shift the start point of the dataset to use a different test/train split'
+                             'for cross validation.')
+    parser.add_argument('--num-workers', type=int, default=8,
+                        help='Dataset workers')
+    parser.add_argument('--batch-size', type=int, default=8,
+                        help='Batch size')
+    parser.add_argument('--epochs', type=int, default=30,
+                        help='Training epochs')
+    parser.add_argument('--batches-per-epoch', type=int, default=1000,
+                        help='Batches per Epoch')
+    parser.add_argument('--val-batches', type=int, default=250,
+                        help='Validation Batches')
+    parser.add_argument('--optim', type=str, default='adam',
+                        help='Optmizer for the training. (adam or SGD)')
 
     # Logging etc.
-    parser.add_argument('--description', type=str, default='', help='Training description')
-    parser.add_argument('--logdir', type=str, default='logs/', help='Log directory')
-    parser.add_argument('--vis', action='store_true', help='Visualise the training process')
-    parser.add_argument('--cpu', dest='force_cpu', action='store_true', default=False, help='force code to run in CPU mode')
+    parser.add_argument('--description', type=str, default='',
+                        help='Training description')
+    parser.add_argument('--logdir', type=str, default='logs/',
+                        help='Log directory')
+    parser.add_argument('--vis', action='store_true',
+                        help='Visualise the training process')
+    parser.add_argument('--cpu', dest='force_cpu', action='store_true', default=False,
+                        help='Force code to run in CPU mode')
 
     args = parser.parse_args()
     return args
@@ -247,8 +274,14 @@ def run():
     # Load the network
     logging.info('Loading Network...')
     input_channels = 1*args.use_depth + 3*args.use_rgb
+    network = get_network(args.network)
+    net = network(
+        input_channels=input_channels,
+        dropout=args.use_dropout,
+        prob=args.dropout_prob,
+        channel_size=args.channel_size
+    )
 
-    net = GenerativeResnet(input_channels=input_channels)
     net = net.to(device)
     logging.info('Done')
 
