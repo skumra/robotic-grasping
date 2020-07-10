b6fb0f3bd8886f347d00817bcc2e2bdc7f6d5f1a
Sulabh Kumra <sulabh@osaro.com>
2020-07-10 15:56:12 -0700

    Added comments for network

--- inference/models/__init__.py
+++ inference/models/__init__.py
@@ -1,14 +1,18 @@
 def get_network(network_name):
     network_name = network_name.lower()
+    # Original GR-ConvNet
     if network_name == 'grconvnet':
         from .grconvnet import GenerativeResnet
         return GenerativeResnet
+    # Configurable GR-ConvNet with multiple dropouts
     elif network_name == 'grconvnet2':
         from .grconvnet2 import GenerativeResnet
         return GenerativeResnet
+    # Configurable GR-ConvNet with dropout at the end
     elif network_name == 'grconvnet3':
         from .grconvnet3 import GenerativeResnet
         return GenerativeResnet
+    # Inverted GR-ConvNet
     elif network_name == 'grconvnet4':
         from .grconvnet4 import GenerativeResnet
         return GenerativeResnet
